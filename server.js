67ddcf76b3bf114ad7022b50f25902db8fe9a12a
maxbittker <maxbittker@gmail.com>
2015-10-13 21:05:29 -0400

    added imagemagick to dither and resize images

--- server.js
+++ server.js
@@ -3,19 +3,41 @@ var Twit = require('twit');
 
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
+var request = require('request');
 var open = require("open");
+var fs = require("fs");
+var gm = require("gm").subClass({
+    imageMagick: true
+});
+count = 0;
+c2 = 0;
 
 function watchTag() {
 
     var stream = T.stream('statuses/filter', {
-        track: ['canada']
+        track: ['food']
     })
 
     stream.on('tweet', function(tweet) {
         try {
-            console.log(tweet.entities.media[0].media_url)
-            open(tweet.entities.media[0].media_url)
-        } catch (e) {}
+            var imgurl = tweet.entities.media[0].media_url
+                // console.log(imgurl)
+            var id = imgurl.split("/")[4]
+                // console.log(id)
+
+            var stream = request(imgurl).pipe(fs.createWriteStream('img/' + count + ".jpg"))
+            count++;
+            stream.on('finish', function() {
+
+                    gm("img/" + c2 + ".jpg").sharpen(3).resize(296).monochrome().write('output/' + count + ".bmp", function(err) {
+                        if (!err) console.log('wrote: ' + id);
+                    });
+                    c2++
+                })
+                // open(imgurl);
+        } catch (e) {
+            // console.log(e)
+        }
     })
 
 
